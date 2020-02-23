8d0139d29db3fab39eddb0c234d729c4abf28dc7
Veteri <veteri@github.noreply.com>
2020-02-23 21:34:53 +0100

    Boilerplate express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("index.js");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("FH Kicker");
+});
+
+app.listen(28960, () => console.log("FH Kicker running on localhost:28960"));
