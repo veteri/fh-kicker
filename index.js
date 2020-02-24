99de88348d11c8fd130f52fc6b6351019193ac29
Veteri <veteri@users.noreply.github.com>
2020-02-24 22:18:47 +0100

    Add static route

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 var path = require('path');
 
+app.use("/static", express.static("public"));
+
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/index.html'));
 });
