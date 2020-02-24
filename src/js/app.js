e309ac2654228306f4be777a6f8ebdccf05d17c2
Veteri <veteri@users.noreply.github.com>
2020-02-25 00:44:45 +0100

    Fixed indent

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,37 @@
 import "../css/style.css";
 
 /* Set this to true if you  
-   are working with webpack */
+are working with webpack */
 const DEV = true;
 
 /**
- * Handles user registration
- * @type {object}
- */
+* Handles user registration
+* @type {object}
+*/
 const registration = {
 
-	/**
-	 * API Path
-	 * @type {string}
-	 */
+    /**
+     * API Path
+     * @type {string}
+     */
     PATH: "/register-user/",
 
-	/**
-	 * Convenience access to all
-	 * used DOM elements from the page
-	 * @type {object}
-	 */
+    /**
+     * Convenience access to all
+     * used DOM elements from the page
+     * @type {object}
+     */
     DOM: {
         matrikelInput: document.querySelector("#textInput"),
         submitBtn: document.querySelector("#submitBtn")
     },
 
-	/**
-	 * Handles a user registration.
-	 * Transmits entered data to the backend
-	 * for further validation and processing
-	 * @returns {undefined}
-	 */
+    /**
+     * Handles a user registration.
+     * Transmits entered data to the backend
+     * for further validation and processing
+     * @returns {undefined}
+     */
     async registrationHandler() {
         const data = {
             email: `${this.DOM.matrikelInput.value}@fh-wedel.de`
@@ -58,27 +58,26 @@ const registration = {
         }
     },
 
-	/**
-	 * Bind all event listeners
-	 * to the DOM elements.
-	 * @returns {undefined}
-	 */
+    /**
+     * Bind all event listeners
+     * to the DOM elements.
+     * @returns {undefined}
+     */
     bindEvents() {
         //Bind submit button click to our registrationHandler
         this.DOM.submitBtn.addEventListener("click", () => this.registrationHandler());
     },
 
-	/**
-	 * Inits the registration object
-	 * @returns {undefined}
-	 */
+    /**
+     * Inits the registration object
+     * @returns {undefined}
+     */
     init() {
         this.bindEvents();
 
-		/* webpack dev server will 
-		 * proxy /api prefixed paths 
-		 * to our backend, see webpack.config.js
-		 */
+        /* webpack dev server will 
+         * proxy /api prefixed paths 
+         * to our backend, see webpack.config.js */
         if (DEV)
             this.PATH = `/api${this.PATH}`;
     }
